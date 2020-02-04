6c617d65fc7daade42af9e2789ec4607bfe0772c
Cristina Camargo Reguera <cristina.cam.reg@gmail.com>
2020-02-04 11:05:58 +0100

    funciona

--- main.py
+++ main.py
@@ -12,38 +12,16 @@ from src.functionsGoT import apiToDf
 from src.functionsGoT import regionName
 
 
-
-
-#csv characters
-characters_csv = pd.read_csv('src/character-deaths.csv')
-characters_csv = characters_csv[['Name', 'Book of Death', 'Gender']]
-characters_csv.Gender = cleanGender(characters_csv.Gender,genders_csv)
-characters_csv['Book of Death']=characters_csv['Book of Death'].fillna(0).astype(int).astype(str)
-#API characters
-characters_df = apiToDf("https://api.got.show/api/book/characters")
-characters_df = characters_df[['name','gender', 'house', 'alive']].rename(columns={'name':'Name','gender':'Gender'})
-#DF characters
-characters_DF = pd.merge_ordered(characters_df,characters_csv, on=['Name','Gender'])
-characters_DF = characters_DF.fillna('-')
-characters_DF['house'] = characters_DF['house'].replace(regex={r'(&apos;)':"'",r'(\[\d\])':''})
-#DF houses
-houses_DF = apiToDf('https://api.got.show/api/book/houses')
-houses_DF = houses_DF[['name','region','overlords']]
-houses_DF['region'] = houses_DF['region'].fillna('Unknown')
-houses_DF['region'] = regionName(houses_DF['region'])
-
-dead = characters_DF[(characters_DF['alive']!=True) & (characters_DF['Book of Death']!=('-'))]
-
-
-def death_house(house,book):
+def death_house(house,book, dead,characters_DF):
 
     for libro in books.items():
         if libro[0] == str(book):
             titulo=libro[1]
-    numero_muertos = dead[(dead['house']==house) & (dead['Book of Death']==book)].shape
-    gente_casa = characters_DF[(characters_DF['house']==house)].shape
-    porc_muertes = int((numero_muertos[0]/gente_casa[0])*100)
-    print(porc_muertes)
+    
+    numero_muertos = dead.loc[(dead['house']==house) & (dead['Book of Death']==str(book))].shape[0]
+    gente_casa = characters_DF[(characters_DF['house']==house)].shape[0]
+    porc_muertes = int((numero_muertos/gente_casa)*100)
+    
     return f'The {house} has {porc_muertes}% of the deaths in "{titulo}" book'
 
 def parser():
@@ -57,16 +35,34 @@ def parser():
     house = args.house
     book = args.book
     
-    return house, book
+    return house[0], book[0]
 
 def main():
-    print(death_house(house,book))
+    house, book = parser()
+    #csv characters
+    characters_csv = pd.read_csv('src/character-deaths.csv')
+    characters_csv = characters_csv[['Name', 'Book of Death', 'Gender']]
+    characters_csv.Gender = cleanGender(characters_csv.Gender,genders_csv)
+    characters_csv['Book of Death']=characters_csv['Book of Death'].fillna(0).astype(int).astype(str)
+    #API characters
+    characters_df = apiToDf("https://api.got.show/api/book/characters")
+    characters_df = characters_df[['name','gender', 'house', 'alive']].rename(columns={'name':'Name','gender':'Gender'})
+    #DF characters
+    characters_DF = pd.merge_ordered(characters_df,characters_csv, on=['Name','Gender'])
+    characters_DF = characters_DF.fillna('-')
+    characters_DF['house'] = characters_DF['house'].replace(regex={r'(&apos;)':"'",r'(\[\d\])':''})
+    #DF houses
+    houses_DF = apiToDf('https://api.got.show/api/book/houses')
+    houses_DF = houses_DF[['name','region','overlords']]
+    houses_DF['region'] = houses_DF['region'].fillna('Unknown')
+    houses_DF['region'] = regionName(houses_DF['region'])
+
+    dead = characters_DF[(characters_DF['alive']!=True) & (characters_DF['Book of Death']!=('-'))]
+    print(death_house(house,book,dead,characters_DF))
 
 
 if __name__=='__main__':
     
-    house, book = parser()
-    
-    main()
+     main()
         
 
