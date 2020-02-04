e5e2b4e114b81d4cdd39e34a0a8fbf237fe50610
Cristina Camargo Reguera <cristina.cam.reg@gmail.com>
2020-02-04 11:41:49 +0100

    presentado

--- main.py
+++ main.py
@@ -12,57 +12,65 @@ from src.functionsGoT import apiToDf
 from src.functionsGoT import regionName
 
 
-def death_house(house,book, dead,characters_DF):
+def death_house(house, book, dead, characters_DF):
 
     for libro in books.items():
         if libro[0] == str(book):
-            titulo=libro[1]
-    
-    numero_muertos = dead.loc[(dead['house']==house) & (dead['Book of Death']==str(book))].shape[0]
-    gente_casa = characters_DF[(characters_DF['house']==house)].shape[0]
+            titulo = libro[1]
+
+    numero_muertos = dead.loc[(dead['house'] == house) & (
+        dead['Book of Death'] == str(book))].shape[0]
+    gente_casa = characters_DF[(characters_DF['house'] == house)].shape[0]
     porc_muertes = int((numero_muertos/gente_casa)*100)
-    
+
     return f'The {house} has {porc_muertes}% of the deaths in "{titulo}" book'
 
+
 def parser():
-    
-    parser = ArgumentParser(description="Porcentaje de muertos por casa en cada libro")
-    
-    parser.add_argument("house",type=str, nargs=1,help='Introducir una casa de GoT')
-    parser.add_argument("book",type=int, nargs=1, help='Introducir el numero del libro que quieras')
+
+    parser = ArgumentParser(
+        description="Porcentaje de muertos por casa en cada libro")
+    parser.add_argument("house", type=str, nargs=1,
+                        help='Introducir una casa de GoT')
+    parser.add_argument("book", type=int, nargs=1,
+                        help='Introducir el numero del libro que quieras')
 
     args = parser.parse_args()
     house = args.house
     book = args.book
-    
+
     return house[0], book[0]
+
 
 def main():
     house, book = parser()
-    #csv characters
+    # csv characters
     characters_csv = pd.read_csv('src/character-deaths.csv')
     characters_csv = characters_csv[['Name', 'Book of Death', 'Gender']]
-    characters_csv.Gender = cleanGender(characters_csv.Gender,genders_csv)
-    characters_csv['Book of Death']=characters_csv['Book of Death'].fillna(0).astype(int).astype(str)
-    #API characters
+    characters_csv.Gender = cleanGender(characters_csv.Gender, genders_csv)
+    characters_csv['Book of Death'] = characters_csv['Book of Death'].fillna(
+        0).astype(int).astype(str)
+    # API characters
     characters_df = apiToDf("https://api.got.show/api/book/characters")
-    characters_df = characters_df[['name','gender', 'house', 'alive']].rename(columns={'name':'Name','gender':'Gender'})
-    #DF characters
-    characters_DF = pd.merge_ordered(characters_df,characters_csv, on=['Name','Gender'])
+    characters_df = characters_df[['name', 'gender', 'house', 'alive']].rename(
+        columns={'name': 'Name', 'gender': 'Gender'})
+    # DF characters
+    characters_DF = pd.merge_ordered(
+        characters_df, characters_csv, on=['Name', 'Gender'])
     characters_DF = characters_DF.fillna('-')
-    characters_DF['house'] = characters_DF['house'].replace(regex={r'(&apos;)':"'",r'(\[\d\])':''})
-    #DF houses
+    characters_DF['house'] = characters_DF['house'].replace(
+        regex={r'(&apos;)': "'", r'(\[\d\])': ''})
+    # DF houses
     houses_DF = apiToDf('https://api.got.show/api/book/houses')
-    houses_DF = houses_DF[['name','region','overlords']]
+    houses_DF = houses_DF[['name', 'region', 'overlords']]
     houses_DF['region'] = houses_DF['region'].fillna('Unknown')
     houses_DF['region'] = regionName(houses_DF['region'])
 
-    dead = characters_DF[(characters_DF['alive']!=True) & (characters_DF['Book of Death']!=('-'))]
-    print(death_house(house,book,dead,characters_DF))
+    dead = characters_DF[(characters_DF['alive'] != True) & (
+        characters_DF['Book of Death'] != ('-'))]
+    print(death_house(house, book, dead, characters_DF))
 
 
-if __name__=='__main__':
-    
-     main()
-        
+if __name__ == '__main__':
 
+    main()
--- src/variab.py
+++ src/variab.py
@@ -1,27 +1,25 @@
 
 genders_csv = {
-                1:'male',
-                0:'female'
-                }
+    1: 'male',
+    0: 'female'
+}
 
-regions={
-        'The Vale':['Vale of Arryn','Vale','The Vale', 'the Vale', 'The Vale of Arryn'],
-        'The North':['North', 'north','The North'],
-        'The Reach':['Reach','the Reach','The Reach'],
-        'Riverlands':['Riverlands','riverlands'],
-        'Westerlands':['Westerlands','westerlands','The Westerlands','the Westerlands'],
-        'Crownlands':['Crownlands','crownlands'],
-        'Stormlands':['stormlands','Stormlands','The Stormlands','the Stormlands'],
-        'Unknown':['']
-        }  
+regions = {
+    'The Vale': ['Vale of Arryn', 'Vale', 'The Vale', 'the Vale', 'The Vale of Arryn'],
+    'The North': ['North', 'north', 'The North'],
+    'The Reach': ['Reach', 'the Reach', 'The Reach'],
+    'Riverlands': ['Riverlands', 'riverlands'],
+    'Westerlands': ['Westerlands', 'westerlands', 'The Westerlands', 'the Westerlands'],
+    'Crownlands': ['Crownlands', 'crownlands'],
+    'Stormlands': ['stormlands', 'Stormlands', 'The Stormlands', 'the Stormlands'],
+    'Unknown': ['']
+}
 
-books={
-        '0':'Before the books',
-        '1':'A Game of Thrones',
-        '2':'A Clash of Kings',
-        '3':'A Storm of Swords',
-        '4':'A Feast for Crows',
-        '5':'A Dance with Dragons'
-        }
-
-
+books = {
+    '0': 'Before the books',
+    '1': 'A Game of Thrones',
+    '2': 'A Clash of Kings',
+    '3': 'A Storm of Swords',
+    '4': 'A Feast for Crows',
+    '5': 'A Dance with Dragons'
+}
